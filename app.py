b447181b07244b7046068c57a716a25e2ba3d186
pdmarc7 <p.marcells@yahoo.com>
2022-03-22 22:15:01 +0000

    commencing design

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import pymysql.cursors
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    connection = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='coldkov-97',
+            database='laravel',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "show tables;"
+        cursor.execute(sql)
+        
+        response = cursor.fetchall()
+        tables = [res['Tables_in_laravel'] for res in response]
+
+    return render_template('index.html', tables=tables)
